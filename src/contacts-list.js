8bf4daccb742edac2bcad56edcfded59b88ab356
Divan Visagie <visagiedivan@gmail.com>
2016-07-02 20:02:27 +0200

    add minor searchbar functionality

--- src/contacts-list.js
+++ src/contacts-list.js
@@ -2,15 +2,32 @@ import React from 'react'
 import Contact from './contact'
 
 class ContactsList extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      search: 'React'
+    }
+  }
+
+  updateSearch(event) {
+    this.setState({search: event.target.value})
+  }
+
   render() {
     return (
-      <ul>
-        {
-          this.props.contacts.map( contact => {
-              return <Contact key={contact.name} contact={contact}/>
-          })
-        }
-      </ul>
+      <div>
+        <ul>
+          {
+            this.props.contacts.map( contact => {
+                return <Contact key={contact.name} contact={contact}/>
+            })
+          }
+        </ul>
+        <input type="text"
+          value={this.state.search}
+          onChange={this.updateSearch.bind(this)}
+        />
+      </div>
     )
   }
 }
