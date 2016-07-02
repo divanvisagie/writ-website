0189a4a1dcb391b9fab7f696e7e642c8feeb0855
Divan Visagie <visagiedivan@gmail.com>
2016-07-02 19:24:06 +0200

    add sub components

---
+++ src/contacts-list.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+class ContactsList extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>Bobby 555 555 5555</li>
+      </ul>
+    )
+  }
+
+}
+
+export default ContactsList
